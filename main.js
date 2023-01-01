ac7043f176bbc82b4373aca48c9efcfd3919c8b5
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2023-01-01 19:45:15 +0530

    create routes in the main.js file

--- main.js
+++ main.js
@@ -15,6 +15,37 @@ app.get('/', (req, res) => {
     res.send("Hello world!");
 })
 
+// let's try a dynamic route
+// app.get("/book", (req, res) => {
+//     res.send("All books");
+// })
+//
+// app.post("/book", (req, res) => {
+//     res.json({ data: "book is stored" });
+// })
+//
+// We can chain these functions like this,
+app
+    .route('/book')
+    .get((req, res) => {
+        res.send("All books");
+    })
+    .post((req, res) => {
+        res.json({ data: "Book is stored" });
+    })
+
+app.get("/book/:id", (req, res) => {
+    res.send(`Single book of ID: ${req.params.id}`);
+})
+
+app.post("/", (req, res) => {
+    res.json({ name: "Sujeewa Sandeepa" });
+})
+
+app.all('/*', (req, res) => {
+    res.send("Page not found!");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is listening at ${PORT}`);
 })
